4256e935e0e61f0c85c1f94f226ec076adcdfcd3
Rob Jinman <jinmanr@gmail.com>
2022-01-29 18:14:31 +0000

    Remove default front page, add header component and empty article service

--- app/src/app/app.component.spec.ts
+++ app/src/app/app.component.spec.ts
@@ -1,15 +1,19 @@
 import { TestBed } from '@angular/core/testing';
+import { MatCardModule } from '@angular/material/card';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatCardModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent
       ],
     }).compileComponents();
   });
@@ -26,10 +30,12 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('deadbeefio');
   });
 
-  it('should render title', () => {
+  it('should render copyright notice', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('deadbeefio app is running!');
+    const currentYear = (new Date()).getUTCFullYear();
+    expect(compiled.querySelector('.footer')?.textContent)
+      .toContain(`Copyright Rob Jinman 2019 - ${currentYear}. All rights reserved.`);
   });
 });
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
