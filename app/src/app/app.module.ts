a49d67ddce35064f006236ee3a9e8813c0978538
Rob Jinman <jinmanr@gmail.com>
2022-02-06 16:48:02 +0000

    Separate module for material components

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,10 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FeedComponent } from './feed/feed.component';
@@ -16,6 +12,7 @@ import { FeaturedComponent } from './landing/featured/featured.component';
 import { LandingComponent } from './landing/landing.component';
 import { LatestComponent } from './landing/latest/latest.component';
 import { NewsItemComponent } from './landing/latest/news-item/news-item.component';
+import { MaterialUiModule } from './material-ui/material-ui.module';
 
 @NgModule({
   declarations: [
@@ -33,10 +30,7 @@ import { NewsItemComponent } from './landing/latest/news-item/news-item.componen
     BrowserAnimationsModule,
     GraphQLModule,
     HttpClientModule,
-    MatCardModule,
-    MatToolbarModule,
-    MatGridListModule,
-    MatIconModule
+    MaterialUiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app/src/app/material-ui/material-ui.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const modules = [
+  MatCardModule,
+  MatToolbarModule,
+  MatGridListModule,
+  MatButtonModule,
+  MatIconModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ...modules
+  ],
+  exports: [
+    ...modules
+  ]
+})
+export class MaterialUiModule { }
